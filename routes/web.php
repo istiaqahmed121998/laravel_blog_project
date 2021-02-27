abe06aeeda41c202b80f81c2ffa3b172fb57d72b
Md Istiaq Ahmed <zenithzonezj@gmail.com>
2021-02-28 03:44:14 +0600

    Auth Added

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/adminpanel', [App\Http\Controllers\Admin\adminController::class, 'index'])->name('admin');
